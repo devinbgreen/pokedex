d9c330d5449528f1ca0d71d3d3a49f20f4ff0f3f
devinbgreen <devinbgreen@gmail.com>
2025-11-30 23:12:08 -0700

    added location caching

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -9,13 +9,14 @@ import (
 
 // Client -
 type Client struct {
-	cache      pokecache.Cache
+	cache      *pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -12,23 +12,54 @@ type cacheEntry struct {
 
 type Cache struct {
 	stuff    map[string]cacheEntry
-	critical sync.Mutex
+	mu       sync.Mutex
+	interval time.Duration
 }
 
-func NewCache(interval time.Duration) {
-	//
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		stuff:    make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go c.reapLoop()
+	return c
 }
 
-func (c *Cache) add(key string, val []byte) error {
-	//
+func (c *Cache) Add(key string, val []byte) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	/*
+			_, exists := c.stuff[key]
+		    if exists {
+		        // maybe overwrite, maybe early-return – your choice for this cache
+		    }*/
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.stuff[key] = entry
 	return nil
 }
 
-func (c *Cache) get(key string) ([]byte, bool) {
-	//
-	return nil, true
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.stuff[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
 }
 
 func (c *Cache) reapLoop() {
-
+	ticker := time.NewTicker(c.interval)
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.stuff {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.stuff, key)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
